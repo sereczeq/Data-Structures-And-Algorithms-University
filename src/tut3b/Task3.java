37eeab252bf17b14db26c127e82185f4c9d0ada8
sereczeq <kubaserdynski+github@gmail.com>
2020-04-01 14:22:15 +0200

    Update Task3.java

--- src/tut3b/Task3.java
+++ src/tut3b/Task3.java
@@ -49,20 +49,24 @@ public class Task3
 	private static class Customer
 	{
 		
+		int id;
 		int time;
 		
-		public Customer()
+		public Customer(int id, int time)
 		{
 			
-			time = new Random().nextInt(10);
+			this.id = id;
+			this.time = new Random().nextInt(time);
+			if (this.time == 0) this.time = 1;
 			
 		}
 		
 		
-		public Customer(int time)
+		@Override
+		public String toString()
 		{
 			
-			this.time = time;
+			return "Customer no." + id + " with time: " + time;
 			
 		}
 		
@@ -73,6 +77,11 @@ public class Task3
 		
 		static UnboundedQueue<Customer> queue = new UnboundedQueue<Customer>();
 		static Official[] office = {new Official("A"), new Official("B"), new Official("C") };
+		static boolean run = true;
+		
+		static int howManyCustomers = 20;
+		static int longestPossibleTaskTime = 5;
+		static int newCustomerAddTime = 10;
 		
 		static Thread customers = new Thread()
 		{
@@ -80,10 +89,21 @@ public class Task3
 			public void run()
 			{
 				
-				for (int x = 0; x < 1000; x++)
+				for (int x = 0; x < howManyCustomers; x++)
 				{
-					queue.enqueue(new Customer());
+					try
+					{
+						TimeUnit.SECONDS.sleep(new Random().nextInt(newCustomerAddTime));
+					}
+					catch (InterruptedException e)
+					{
+						e.printStackTrace();
+					}
+					Customer temp = new Customer(x, longestPossibleTaskTime);
+					queue.enqueue(temp);
+					System.out.println("[QUEUE:] added " + temp);
 				}
+				run = false;
 				
 			}
 			
@@ -95,31 +115,32 @@ public class Task3
 			public void run()
 			{
 				
-				while (!queue.isEmpty())
+				while (run || !queue.isEmpty())
 				{
+					// This updates the officials time
 					try
 					{
-						// added this line because it's easier to see queue in real time
 						TimeUnit.SECONDS.sleep(1);
 					}
 					catch (InterruptedException e)
 					{
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 					for (Official official : office) official.tick();
+					
+					// This assigns new customers to officials
 					for (Official official : office)
 					{
-						if (official.isFree())
+						if (official.isFree() && !queue.isEmpty())
 						{
 							Customer customer = queue.dequeue();
 							official.addTask(customer);
-							System.out
-									.println("Official " + official.name + " took customer with time " + customer.time);
+							System.out.println("[OFFICE:] Official " + official.name + " took " + customer);
 						}
 					}
 					
 				}
+				System.out.println("\n\n\t\t\tFINISHED!");
 				
 			}
 			
@@ -128,8 +149,18 @@ public class Task3
 		public static void main(String[] args)
 		{
 			
-			customers.run();
-			officials.run();
+			/*
+			 * Time updates every second
+			 * 
+			 * This variables may be change to see different results
+			 * 
+			 */
+			howManyCustomers = 20;
+			longestPossibleTaskTime = 7;
+			newCustomerAddTime = 3;
+			
+			customers.start();
+			officials.start();
 			
 		}
 		
