e28af873d1767d5e0ef68947725012d566b3294b
sereczeq <kubaserdynski+github@gmail.com>
2020-04-20 12:09:17 +0200

    Update TwoWayCycledOrderedListWithSentinel.java

--- src/lab6/TwoWayCycledOrderedListWithSentinel.java
+++ src/lab6/TwoWayCycledOrderedListWithSentinel.java
@@ -46,6 +46,15 @@ interface IList<E> extends Iterable<E>
 public class TwoWayCycledOrderedListWithSentinel<E> implements IList<E>
 {
 	
+	public static void main(String[] args)
+	{
+		
+		System.out.println("start");
+		int array[] = new int[] {1, 3, 5, 2, 4 };
+		Document.iterativeMergeSort(array);
+		
+	}
+	
 	private class Element
 	{
 		
@@ -704,17 +713,134 @@ class Document
 	}
 	
 	
-	public void iterativeMergeSort(int[] arr)
+	public static void iterativeMergeSort(int[] arr)
 	{
 		
 		showArray(arr);
-		/*
-		 * Let's sort every two elements with each other, then every for elements, etc
-		 */
-		
-		for (int subArraySize = 1, position = 0; subArraySize < arr.length; subArraySize *= 2)
+		int[] array = new int[arr.length];
+		int newPos = 0;
+		int pos1 = 0;
+		int pos2 = 0;
+		int oldPos = 0;
+		int size = 0;
+		for (size = 1; size < arr.length / 2; size *= 2)
 		{
-			if (arr[position] < arr[position + 1]) swap(arr, position, position + 1);
+			for (oldPos = 0; oldPos < arr.length - size; oldPos += size * 2)
+			{
+				while (pos1 < size || pos2 < size)
+				{
+					if (arr[oldPos + pos1] < arr[oldPos + size + pos2] || pos2 == size)
+						array[newPos++] = arr[oldPos + pos1++];
+					else array[newPos++] = arr[oldPos + size + pos2++];
+				}
+			}
+			showArray(array);
+		}
+		
+	}
+	
+	
+	void mergeSort(int arr[], int n)
+	{
+		
+		// For current size of subarrays to
+		// be merged curr_size varies from
+		// 1 to n/2
+		int curr_size;
+		
+		// For picking starting index of
+		// left subarray to be merged
+		int left_start;
+		
+		// Merge subarrays in bottom up
+		// manner. First merge subarrays
+		// of size 1 to create sorted
+		// subarrays of size 2, then merge
+		// subarrays of size 2 to create
+		// sorted subarrays of size 4, and
+		// so on.
+		for (curr_size = 1; curr_size <= n - 1; curr_size = 2 * curr_size)
+		{
+			
+			// Pick starting point of different
+			// subarrays of current size
+			for (left_start = 0; left_start < n - 1; left_start += 2 * curr_size)
+			{
+				// Find ending point of left
+				// subarray. mid+1 is starting
+				// point of right
+				int mid = Math.min(left_start + curr_size - 1, n - 1);
+				
+				int right_end = Math.min(left_start + 2 * curr_size - 1, n - 1);
+				
+				// Merge Subarrays arr[left_start...mid]
+				// & arr[mid+1...right_end]
+				merge(arr, left_start, mid, right_end);
+			}
+		}
+		
+	}
+	
+	
+	/*
+	 * Function to merge the two haves arr[l..m] and arr[m+1..r] of array arr[]
+	 */
+	static void merge(int arr[], int leftStart, int leftEnd, int rightEnd)
+	{
+		
+		int i, j, k;
+		int n1length = leftEnd - leftStart + 1;
+		int n2length = rightEnd - leftEnd;
+		
+		/* create temp arrays */
+		int L[] = new int[n1length];
+		int R[] = new int[n2length];
+		
+		/*
+		 * Copy data to temp arrays L[] and R[]
+		 */
+		for (i = 0; i < n1length; i++) L[i] = arr[leftStart + i];
+		for (j = 0; j < n2length; j++) R[j] = arr[leftEnd + 1 + j];
+		
+		/*
+		 * Merge the temp arrays back into arr[l..r]
+		 */
+		i = 0;
+		j = 0;
+		k = leftStart;
+		while (i < n1length && j < n2length)
+		{
+			if (L[i] <= R[j])
+			{
+				arr[k] = L[i];
+				i++;
+			}
+			else
+			{
+				arr[k] = R[j];
+				j++;
+			}
+			k++;
+		}
+		
+		/*
+		 * Copy the remaining elements of L[], if there are any
+		 */
+		while (i < n1length)
+		{
+			arr[k] = L[i];
+			i++;
+			k++;
+		}
+		
+		/*
+		 * Copy the remaining elements of R[], if there are any
+		 */
+		while (j < n2length)
+		{
+			arr[k] = R[j];
+			j++;
+			k++;
 		}
 		
 	}
